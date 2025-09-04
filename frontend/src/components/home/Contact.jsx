2c832d9b65e7128b1b84faa1583150dacdae2728
joonsol <71648638+joonsol@users.noreply.github.com>
2025-09-04 18:06:57 +0900

    .

--- frontend/src/components/home/Contact.jsx
+++ frontend/src/components/home/Contact.jsx
@@ -5,8 +5,10 @@ const Contact = () => {
   return (
     <div className='inner contact-inner'>
       <h1 className="tit">
-        <span className="star-spin"><i className="star">✱</i></span>
         aboutMe
+        <span className="star-spin">
+          <i className="star">✱</i>
+        </span>
       </h1>
       <div className="contact-wrapper">
         <form className='contact-form'>
@@ -14,19 +16,19 @@ const Contact = () => {
             <li>
               <label htmlFor="name" className='label'>이름</label>
               <div className="field">
-                <input type="text" placeholder='홍길동'/>
+                <input type="text" placeholder='홍길동' />
               </div>
             </li>
             <li>
               <label htmlFor="email" className='label'>이메일</label>
               <div className="field">
-                <input type="email" placeholder='example@naver.com'/>
+                <input type="email" placeholder='example@naver.com' />
               </div>
             </li>
             <li>
               <label htmlFor="phone" className='label'>연락처</label>
               <div className="field">
-                <input type="tel" placeholder='010-1234-5678'/>
+                <input type="tel" placeholder='010-1234-5678' />
               </div>
             </li>
             <li>
@@ -55,7 +57,7 @@ const Contact = () => {
           <li>
             <strong className="label">채널</strong>
             <div className="contact-chips">
-              
+
             </div>
           </li>
         </ul>
